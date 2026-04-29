aa71b755115d1369fcf673d9bcba0063e890b667
nvu1608 <uynguyenvan1608@gmail.com>
2026-04-29 17:49:39 +0700

    remove toolchain in pc

--- arm_gcc/arm-gnu-toolchain-15.2.rel1-aarch64-arm-none-eabi/lib/gcc/arm-none-eabi/15.2.1/plugin/include/configargs.h
+++
@@ -1,7 +0,0 @@
-/* Generated automatically. */
-static const char configuration_arguments[] = "/tmp/jenkins-e59aa687/workspace/GNU-toolchain/arm-15/src/gcc/configure --target=arm-none-eabi --prefix=/tmp/jenkins-e59aa687/workspace/GNU-toolchain/arm-15/build-arm-none-eabi/install --with-gmp=/tmp/jenkins-e59aa687/workspace/GNU-toolchain/arm-15/build-arm-none-eabi/host-tools --with-mpfr=/tmp/jenkins-e59aa687/workspace/GNU-toolchain/arm-15/build-arm-none-eabi/host-tools --with-mpc=/tmp/jenkins-e59aa687/workspace/GNU-toolchain/arm-15/build-arm-none-eabi/host-tools --with-isl=/tmp/jenkins-e59aa687/workspace/GNU-toolchain/arm-15/build-arm-none-eabi/host-tools --disable-shared --disable-nls --disable-threads --enable-checking=release --enable-languages=c,c++,fortran --with-newlib --with-gnu-as --with-headers=yes --with-gnu-ld --with-native-system-header-dir=/include --with-sysroot=/tmp/jenkins-e59aa687/workspace/GNU-toolchain/arm-15/build-arm-none-eabi/install/arm-none-eabi --with-bugurl=https://gitlab.arm.com/tooling/gnu-devtools-for-arm/-/issues/ --with-multilib-list=aprofile,rmprofile --with-pkgversion='Arm GNU Toolchain 15.2.Rel1 (Build arm-15.86)'";
-static const char thread_model[] = "single";
-
-static const struct {
-  const char *name, *value;
-} configure_default_options[] = { { "cpu", "arm7tdmi" }, { "float", "soft" } };
